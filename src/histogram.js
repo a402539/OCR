3f33c8389ba2b1c97651f5adf9602de7d90c0f62
OriginalSin <saleks@scanex.ru>
2013-09-17 23:25:12 +0400

    Алгоритм вроде забацал правда не видно результата

--- src/histogram.js
+++ src/histogram.js
@@ -38,56 +38,117 @@
             for (var i = 0, len = imgData.data.length; i < len; i++) {
               if (imgData.data[i] > from && imgData.data[i] < to) imgData.data[i] = 255;
 			}*/
-            this.ctx.putImageData(imgData, begX, begY, begX, begY, ww, hh);
+            this.ctx.putImageData(imgData, begX, begY, 0, 0, ww, hh);
 			return len;
 		}
 		,'findLetters': function(ph)	{	    	// поиск символов
             var begX = ph['x'] || 0;
             var begY = ph['y'] || 0;
-            var from = ph['from'] || 50;
-            var to = ph['to'] || 255;
             var ww = ph['width'] || this.width; // в пикселах
             var hh = ph['height'] || this.height;
             var imgData = this.ctx.getImageData(begX, begY, ww, hh);
-            var out = ph;
-            var notNull=0; // число непустых пикселов
-            var Letters = []; // массив символов
-            var Letter=[]; // символ - временный массив для хранения текущего символа
-            var LetterIndex=-1; // временный индекс символов соседних пикселей
-            var puxel=[]; // вспомогательный двумерный массив - для каждого пиксела - индекс символа в массиве letters
-            for (var i = 0, len = imgData.data.length; i < len; i+=4) { // каждый пиксел - 4 элемента
-            	 // всего колонок ww
-            	 // всего строк -len%(-4*ww)
-            	 stroka=i%(4*ww); // номер строки, нумерация с 0
-            	 kolonka=(i-4*ww*stroka)%4; // номер колонки, нумерация с 0
-               if (imgData.data[i] < 255) {
-                notNull++;
-                if (stroka==0){
-                	if(kolonka==0){
-                		puxel[0][0]=0; Letters[0][0]=[0,0];
-                	}
-                	else{
-                		LetterIndex=puxel[0,kolonka-1];
-                		if (LetterIndex>=0){
-                			Letters[0][kolonka]=[0,0];
-                		}
-                	}
-                		
-                // вверху слева
-                LetterIndex=puxel[?(stroka==0,0,stroka-1),?(kolonka==0,0,kolonka-1)];
-                // вверху прямо
-                LetterIndex2=puxel[?(stroka==0,0,stroka-1),kolonka];
-                if(LetterIndex<0){LettreIndex=LetterIndex2}
-                else{
-                	if(LetterIndex2>=0){
-                		// Содержимое массива Letters[LetterIndex2] добавить к массиву Letters[LetterIndex]
-                		}
+
+            var Letters = {}; // Хэш символов
+            var num = 0; // количество символов
+            var puxel = []; // вспомогательный двумерный массив - для каждого пиксела - индекс символа в массиве letters
+
+            var smvConcat = function(raw, col, to) {
+                var ind = puxel[raw][col];
+if(ind === to) {
+var tt =1;
+return;
+}
+
+                var simv = Letters[ind];
+                for (var j = 0, len1 = simv['coordinates'].length; j < len1; j++) {
+                    var arr = simv['coordinates'][j];
+                    puxel[arr[1]][arr[0]] = to;
                 }
-               }
+                Letters[to]['coordinates'] = simv['coordinates'].concat(Letters[to]['coordinates']);
+                delete Letters[ind];
+                //puxel[raw][col] = to;
+            }
+
+            for (var i = 0, w4 = 4*ww, len = imgData.data.length; i < len; i+=4) { // каждый пиксел - 4 элемента
+                var zn = imgData.data[i];
+                if (zn === 255) continue;   // если белая точка переходим к следующей
+            	 // всего колонок ww
+            	 // всего строк hh
+            	var stroka = Math.floor(i / w4),                    // номер строки, нумерация с 0
+                    kolonka = Math.floor(i / 4 - ww * stroka),    // номер колонки, нумерация с 0
+                    ind = null;
+                    
+                num++;
+                Letters[num] = {
+                    'type': 'MULTIPOINT'
+                    ,'coordinates': [[kolonka, stroka]]
+                };
+                if(!puxel[stroka]) puxel[stroka] = [];
+                puxel[stroka][kolonka] = num;
+
+                if (kolonka > 0) {
+                    if (imgData.data[i - 4] < 255) {                // если точка левее текущей не белая
+                        smvConcat(stroka, kolonka - 1, num);
+                    }
+                }
+                if (stroka > 0) {
+                    var ind1 = null;
+                    if (imgData.data[i - w4] < 255) {               // если точка выше текущей не белая
+                        smvConcat(stroka - 1, kolonka, num);
+                    }
+                    if (imgData.data[i - w4 + 4] < 255) {           // если точка выше+правее текущей не белая
+                        smvConcat(stroka - 1, kolonka + 1, num);
+                    }
+                    if (imgData.data[i - w4 - 4] < 255) {           // если точка выше+левее текущей не белая
+                        smvConcat(stroka - 1, kolonka - 1, num);
+                    }
+                }
  			}
-            out['notNull'] = notNull;
-			return out;
+			return Letters;
 		}
+		,'normalization': function(Letters)	{	    	// нормализация
+            for (var key in Letters) {
+                var simv = Letters[key];
+                simv['bounds'] = window.abcAPI['utils'].bounds(simv['coordinates']);
+                
+                for (var j = 0, len1 = simv['coordinates'].length; j < len1; j++) {
+                    var arr = simv['coordinates'][j];
+                    arr[0] -= simv['bounds'].min.x;
+                    arr[1] -= simv['bounds'].min.y;
+                }
+            }
+            return Letters;
+		}
+		,'drawResult': function(ph)	{	    	// нарисуем символы
+            var begX = ph['x'] || 0;
+            var begY = ph['y'] || 0;
+            var ww = ph['width'] || this.width; // в пикселах
+            var w4 = 4*ww;
+            var hh = ph['height'] || this.height;
+
+            var Letters = this.normalization(ph['res']);
+            var imgDataDraw = this.ctx.getImageData(ph['tx'], ph['ty'], 100, 400);
+            for (var i = 0, len1 = imgDataDraw.data.length; i < len1; i+=4) {       // Покрасим белым поле вывода
+                imgDataDraw.data[i] = 
+                imgDataDraw.data[i+1] = 
+                imgDataDraw.data[i+2] = 
+                imgDataDraw.data[i+3] = 255;
+            }
+            var i = 0;
+            for (var key in Letters) {
+                var simv = Letters[key];
+                var add = 100 * i++;
+                for (var j = 0, len1 = simv['coordinates'].length; j < len1; j++) {
+                    var arr = simv['coordinates'][j];
+                    var indRes = (arr[1] + add) * 100 + arr[0];
+                    imgDataDraw.data[indRes] = 
+                    imgDataDraw.data[indRes + 1] = 
+                    imgDataDraw.data[indRes + 2] = 0;
+                }
+           }
+           this.ctx.putImageData(imgDataDraw, ph['tx'], ph['ty'], 0, 0, 100, 400);
+		}
+        
 	};
 
 	//расширяем namespace
--- src/utils.js
+++ src/utils.js
@@ -14,10 +14,44 @@ console.log('run' , item);
 			chkTimer();
 			return items.length;
 		}
-		,'getCounts': function()	{				// получить размер очереди + колич.выполняющихся запросов
-//console.log('getCounts' , curCount, items.length); 
-			return items.length + (curCount > 0 ? curCount : 0);
-		}
+        ,
+        'bounds': function(arr) {							// получить bounds массива точек
+            var res = {
+                min: {
+                    x: Number.MAX_VALUE,
+                    y: Number.MAX_VALUE
+                },
+                max: {
+                    x: -Number.MAX_VALUE,
+                    y: -Number.MAX_VALUE
+                },
+                extend: function(x, y) {
+                    if (x < this.min.x) this.min.x = x;
+                    if (x > this.max.x) this.max.x = x;
+                    if (y < this.min.y) this.min.y = y;
+                    if (y > this.max.y) this.max.y = y;
+                    return this;
+                },
+                extendArray: function(arr) {
+                    if (!arr) { return this };
+                    for(var i=0, len=arr.length; i<len; i++) {
+                        this.extend(arr[i][0], arr[i][1]);
+                    }
+                    return this;
+                },
+                intersects: function (bounds, dx, dy) { // (Bounds, dx, dy) -> Boolean
+                    var min = this.min,
+                        max = this.max,
+                        dx = dx || 0,
+                        dy = dy || 0,
+                        min2 = bounds.min,
+                        max2 = bounds.max;
+                    return max2.x + dx >= min.x && min2.x - dx <= max.x && max2.y + dy >= min.y && min2.y - dy <= max.y;
+                }
+            };
+            
+            return res.extendArray(arr);
+        }
 	};
 
 	//расширяем namespace
