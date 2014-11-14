e835c6c59cb61ec8c70261f1ac2f8282c9ee08d6
OriginalSin <saleks@scanex.ru>
2014-11-14 19:21:26 +0300

    отладка

---
+++ build/ocr-dev.js
@@ -0,0 +1,28 @@
+﻿(function() {
+
+    var depsFilename = 'deps.js';
+
+	function getScriptPath() {
+		var scripts = document.getElementsByTagName('script');
+		for (var i = 0; i < scripts.length; i++) {
+			var src = scripts[i].src;
+			if (src) {
+				var res = src.match(/^(.*)ocr-dev\.js/);
+				if (res) {
+					return res[1];
+				}
+			}
+		}
+	}
+
+	var basePath = getScriptPath();
+    
+    window.devOnLoad = function(deps) {
+        var srcPath = basePath + '../src/';
+        for (var i = 0; i < deps.length; i++) {
+            document.writeln("<script src='" + srcPath + deps[i] + "'></script>");
+        }
+    }
+    
+    document.writeln("<script src='" + basePath + depsFilename + "'></script>");
+})();
